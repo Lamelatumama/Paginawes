e68b8a5dbd3bc5fa766d2757f6487838e6f212ed
yo <no@gmail.com>
2025-06-01 18:44:33 +0200

    Añadida imagen de la situación en cocina Añadida descripción interesante en la página Nosotros para el equipo de cocina Quitado el link de Youtube que no funcionaba

--- Nosotros.php
+++ Nosotros.php
@@ -39,10 +39,10 @@ require_once 'sesion.php';
                 </div>
             </div>
             <div class="about-item">
-                <iframe src="https://www.youtube.com/embed/VIDEO_ID_AQUI" frameborder="0" allowfullscreen></iframe>
+                <img src="img/Nosotros/maxresdefault.jpg" alt="Imagen del equipo 3">
                 <div class="text-content">
                     <h3>Conoce a Nuestro Equipo</h3>
-                    <p>Nuestro equipo está dedicado a brindar un servicio excepcional y crear un ambiente acogedor para todos nuestros clientes.</p>
+                    <p>Nuestro equipo está dedicado a brindar un servicio excepcional y crear un ambiente acogedor para todos nuestros clientes, estamos comprometidos con la excelencia culinaria, donde cada detalle cuenta. Combinamos técnica, pasión y experiencia para ofrecerte platos de la más alta calidad, preparados con precisión y dedicación en cada paso del proceso."</p>
                 </div>
             </div>
             <div class="about-item">
